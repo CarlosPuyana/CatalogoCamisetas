bfc3fb67ea630f25d08f2b1b31dbb76fd546207b
Carlos Puyana <carlospuyana22@gmail.com>
2023-08-20 12:36:07 +0200

    Añadido ligas y equipos a la liga en las categorias

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import supabase from '../supabase.ts';
 
 const Sidebar: React.FC = () => {
-  const categorias = ['Categoria 1', 'Categoria 2', 'Categoria 3']; // Agrega las categorías aquí
+  const [ligas, setLigas] = useState<string[]>([]);
+  const [equipos, setEquipos] = useState<string[]>([]);
+
+  useEffect(() => {
+    async function fetchCategorias() {
+      try {
+        const { data: ligasData, error: ligasError } = await supabase.from('Camisetas').select('liga');
+        const { data: equiposData, error: equiposError } = await supabase.from('Camisetas').select('equipo');
+
+        if (ligasError || equiposError) throw ligasError || equiposError;
+
+        if (ligasData) {
+          const uniqueLigas = [...new Set(ligasData.map((item) => item.liga))];
+          console.log(uniqueLigas);
+          setLigas(uniqueLigas);
+        }
+
+        if (equiposData) {
+          const uniqueEquipos = [...new Set(equiposData.map((item) => item.equipo))];
+          console.log(uniqueEquipos);
+          setEquipos(uniqueEquipos);
+        }
+      } catch (error) {
+        console.error('Error fetching categorías', error);
+      }
+    }
+
+    fetchCategorias();
+  }, []);
+
+  const categorias = [...ligas, ...equipos]; // Combinar ligas y equipos
+  console.log(categorias);
 
   return (
     <div className="bg-light" style={{ width: '20%', minHeight: '100vh' }}>
