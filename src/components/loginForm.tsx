6126409db601c1f831ce87b61c57671518399f6b
Carlos Puyana <carlospuyana22@gmail.com>
2023-08-25 16:32:46 +0200

    Añadido login a la web

--- src/components/loginForm.tsx
+++ src/components/loginForm.tsx
@@ -1,26 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { auth } from '../supabase.ts';
 
 const LoginForm: React.FC = () => {
+  const navigate = useNavigate();
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function checkSession() {
+      const { data, error } = await auth.getSession();
+      if (data.session && !error) {
+        navigate('/'); // Si ya hay una sesión activa, redirigir a la página de inicio
+      }
+    }
+    checkSession();
+  }, []); // El segundo argumento vacío asegura que esto solo se ejecute una vez al montar el componente
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
 
     try {
-        const result = await auth.signUp({ email, password });
+      const { error } = await auth.signInWithPassword({ email, password });
 
-        if (result.error) {
-            setError(result.error.message);
-            return;
-          }
+      if (error) {
+        setError(error.message);
+        return;
+      }
 
-        console.log('User logged in successfully:', result.data.user);
-      // Redirigir al usuario al Home
-
-        return result;
+      console.log('User logged in successfully');
+      navigate('/');
     } catch (error) {
       console.error('Error logging in:', error);
       setError('Error logging in');
@@ -33,11 +44,21 @@ const LoginForm: React.FC = () => {
       <form onSubmit={handleLogin}>
         <div>
           <label>Email:</label>
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
         </div>
         <div>
           <label>Password:</label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
         </div>
         <button type="submit">Login</button>
       </form>
