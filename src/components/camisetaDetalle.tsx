f263ce19a4a48a3b2f2c91d3fe4b5033f3454714
AsecPineda <pinedaseco123@gmail.com>
2023-08-27 20:12:14 +0200

    añadir modal cuando clicas imagen en detalles

--- src/components/camisetaDetalle.tsx
+++ src/components/camisetaDetalle.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Sidebar from '../components/NavBars/sidebar.tsx';
-import { Container } from 'react-bootstrap';
+import { Button, Container, Modal } from 'react-bootstrap';
 import { useParams, Link } from 'react-router-dom';
 import { ICamisetaDetalleProps } from '../Interfaces/camisetas.ts';
 import TopBar from './NavBars/topBar.tsx';
@@ -10,9 +10,9 @@ const CamisetaDetalle: React.FC<ICamisetaDetalleProps> = ({ camisetas }) => {
     const { id } = useParams();
     const camiseta = camisetas.find((c) => c.id === Number(id));
 
-    useEffect(() => {
+    const [showModal, setShowModal] = useState(false);
 
-    }, []);
+    useEffect(() => { }, []);
 
     if (!camiseta) {
         return <p>No existe</p>;
@@ -20,18 +20,33 @@ const CamisetaDetalle: React.FC<ICamisetaDetalleProps> = ({ camisetas }) => {
 
     const imagenesSeparadas = camiseta.imagen.split(' | ');
 
+    const abrirModal = () => {
+        setShowModal(true);
+    };
+
+    const cerrarModal = () => {
+        setShowModal(false);
+    };
+
     return (
         <div className="d-flex">
-            <Sidebar camisetas={camisetas} onEquipoSelected={function (equipo: string): void {
-                throw new Error('Function not implemented.');
-            }} />
+            <Sidebar
+                camisetas={camisetas}
+                onEquipoSelected={(equipo: string): void => {
+                    throw new Error('Function not implemented.');
+                }}
+            />
             <div className="d-flex flex-column align-items-end w-80">
                 <TopBar />
                 <Container className="mt-5" style={{ width: '80%' }}>
                     <div className="card mb-3">
                         <div className="row g-0">
                             <div className="col-md-2">
-                                <img src={camiseta.imagen} className="img-fluid rounded-start camiseta-image" alt={camiseta.nombre} />
+                                <img
+                                    src={camiseta.imagen}
+                                    className="img-fluid rounded-start camiseta-image"
+                                    alt={camiseta.nombre}
+                                />
                             </div>
                             <div className="col-md-4">
                                 <div className="card-body">
@@ -40,18 +55,61 @@ const CamisetaDetalle: React.FC<ICamisetaDetalleProps> = ({ camisetas }) => {
                             </div>
                         </div>
                     </div>
-                    <div className="additional-photos">
-                        {/* Mapea y muestra las fotos adicionales aquí */}
+                    <div>
                         {imagenesSeparadas.map((imagen, index) => (
-                            <img key={index} src={imagen} alt={`Imagen ${index + 2}`} />
+                            <img key={index} src={imagen} alt={`Imagen ${index + 2}`} onClick={abrirModal} />
                         ))}
                     </div>
                     <br></br>
                     <br></br>
-                <Link to="/" className="back-link">Volver</Link>
+                    <Link to="/" className="back-link">
+                        Volver
+                    </Link>
+                    {/**MODAL */}
+                    <ImagenesModal
+                        imagenes={imagenesSeparadas}
+                        initialIndex={0}
+                        show={showModal}
+                        onHide={cerrarModal}
+                    />
                 </Container>
             </div>
         </div>
+    );
+};
+
+const ImagenesModal = ({ imagenes, initialIndex, show, onHide }) => {
+    const [currentIndex, setCurrentIndex] = useState(initialIndex);
+
+    const imagenAnterior = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === 0 ? imagenes.length - 1 : prevIndex - 1));
+    };
+
+    const siguienteImagen = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === imagenes.length - 1 ? 0 : prevIndex + 1));
+    };
+
+    return (
+        <Modal show={show} onHide={onHide}>
+            <Modal.Body>
+                <img
+                    src={imagenes[currentIndex]}
+                    alt={`${currentIndex + 1}`}
+                    style={{ maxWidth: '100%', height: 'auto' }} // tamanio img
+                />
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={imagenAnterior}>
+                    Anterior
+                </Button>
+                <Button variant="secondary" onClick={siguienteImagen}>
+                    Siguiente
+                </Button>
+                <Button variant="primary" onClick={onHide}>
+                    Cerrar
+                </Button>
+            </Modal.Footer>
+        </Modal>
     );
 };
 
