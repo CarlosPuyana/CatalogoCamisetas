63fe5dca0cb069994c29aa95734181570061d83c
AsecPineda <pinedaseco123@gmail.com>
2023-08-28 20:35:31 +0200

    mejorar estilo de sidebar

--- src/components/NavBars/sidebar.tsx
+++ src/components/NavBars/sidebar.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Accordion } from 'react-bootstrap';
 import { ICategoria } from '../../Interfaces/categorias.ts';
 import { ITeam } from '../../Interfaces/teams.ts';
+import '../../css/sidebar.css';
 
 interface SidebarProps {
   teams: ITeam[],
@@ -13,39 +14,41 @@ const Sidebar: React.FC<SidebarProps> = ({ teams, categorias, onEquipoSelected }
   let i = 0;
 
   useEffect(() => {
-    
+
   }, []);
 
-  
+
   return (
-    <div>
+    <div className="sidebar-completo d-flex flex-column vh-100 justify-content-center align-items-center">
       <div className="logo-container">
         <img src='' alt="Logo" className="logo" />
         <h2>Camis365</h2>
       </div>
-      <Accordion>
-        {categorias.map((liga) => {
-          const equiposLiga = teams.filter(q => q.categoria_id === liga.id);
-          const categoriasNoTeams =
-          liga.categoria === 'Retro' || liga.categoria === 'Kids' ? (
-            <Accordion.Header onClick={() => onEquipoSelected(liga.categoria)}>
-              {liga.categoria}
-            </Accordion.Header>
-          ) : (
-            <Accordion.Header>{liga.categoria}</Accordion.Header>
-          );
-          return (
-            <Accordion.Item key={liga.categoria} eventKey={++i + ""}>
-              {categoriasNoTeams}
-              {equiposLiga.map((equipo) => (
-                <Accordion.Body key={equipo.team} onClick={() => onEquipoSelected(equipo.team)}>
-                  {equipo.team}
-                </Accordion.Body>
-              ))}
-            </Accordion.Item>
-          );
-        })}
-      </Accordion>
+      <div className="accordeon">
+        <Accordion>
+          {categorias.map((liga) => {
+            const equiposLiga = teams.filter(q => q.categoria_id === liga.id);
+            const categoriasNoTeams =
+              liga.categoria === 'Retro' || liga.categoria === 'Kids' ? (
+                <Accordion.Header onClick={() => onEquipoSelected(liga.categoria)}>
+                  {liga.categoria}
+                </Accordion.Header>
+              ) : (
+                <Accordion.Header>{liga.categoria}</Accordion.Header>
+              );
+            return (
+              <Accordion.Item key={liga.categoria} eventKey={++i + ""}>
+                {categoriasNoTeams}
+                {equiposLiga.map((equipo) => (
+                  <Accordion.Body key={equipo.team} onClick={() => onEquipoSelected(equipo.team)}>
+                    {equipo.team}
+                  </Accordion.Body>
+                ))}
+              </Accordion.Item>
+            );
+          })}
+        </Accordion>
+      </div>
     </div>
   );
 };
